93194817274b516aebfb7cadcf2d3b8c9bb2f4b6
Ricardo Almeida <ricardo.almeida@gonow.com.br>
2009-10-09 18:23:41 -0300

    incluindo campos do comprador

--- lib/braspag/rails.rb
+++ lib/braspag/rails.rb
@@ -13,20 +13,71 @@ module Braspag
       end
       def braspag_info_form_for(route)
         form_tag route do
-          html = label_tag 'Nome Comprador'
-          html.concat text_field_tag 'NOME'
-          html.concat '<br />'
-          html.concat label_tag 'Numero do Pedido'
-          html.concat text_field_tag 'VENDAID'
-          html.concat '<br />'
-          html.concat label_tag 'Valor Total'
-          html.concat text_field_tag 'VALOR'
-          html.concat '<br />'
-          html.concat label_tag 'Forma de Pagamento'
-          html.concat text_field_tag 'CODPAGAMENTO'
-          html.concat '<p>'
-          html.concat submit_tag 'Prosseguir'
-          html.concat '</p>'
+          template = ERB.new <<-EOF
+            <%= label_tag 'Numero do Pedido' %>
+            <%= text_field_tag 'VENDAID' %>
+            <br />
+            <%= label_tag 'Valor Total' %>
+            <%= text_field_tag 'VALOR' %>
+            <br /><br />
+
+            <%= label_tag 'Informações do Comprador' %>
+            <br />
+            <%= label_tag 'Comprador' %>
+            <%= text_field_tag 'NOME' %>
+            <br />
+            <%= label_tag 'CPF' %>
+            <%= text_field_tag 'CPF' %>
+            <br />
+            <%= label_tag 'Nome da Empresa' %>
+            <%= text_field_tag 'RAZAO_PJ' %>
+            <br />
+            <%= label_tag 'Numero do CNPJ' %>
+            <%= text_field_tag 'CNPJ' %>
+            <br />
+            <%= label_tag 'Logradouro (ex. Rua, Avenida, Estrada ...)' %>
+            <%= text_field_tag 'LOGRADOURO' %>
+            <br />
+            <%= label_tag 'Endereço' %>
+            <%= text_field_tag 'ENDERECO' %>
+            <br />
+            <%= label_tag 'Número' %>
+            <%= text_field_tag 'NUMERO' %>
+            <br />
+            <%= label_tag 'Complemento' %>
+            <%= text_field_tag 'COMPLEMENTO' %>
+            <br />
+            <%= label_tag 'Bairro' %>
+            <%= text_field_tag 'BAIRRO' %>
+            <br />
+            <%= label_tag 'Cidade' %>
+            <%= text_field_tag 'CIDADE' %>
+            <br />
+            <%= label_tag 'CEP' %>
+            <%= text_field_tag 'CPF' %>
+            <br />
+            <%= label_tag 'Estado (UF)' %>
+            <%= text_field_tag 'ESTADO' %>
+            <br />
+            <%= label_tag 'País' %>
+            <%= text_field_tag 'PAIS' %>
+            <br />
+            <%= label_tag 'Data Nascimento' %>
+            <%= text_field_tag 'NASC' %>
+            <br />
+
+            <%= label_tag 'Estado Civil' %>
+            <%= text_field_tag 'ESTCIVIL' %>
+            <br />
+
+
+            <%= label_tag 'Forma de Pagamento' %>
+            <%= text_field_tag 'CODPAGAMENTO' %>
+            <p>
+            <%= submit_tag 'Prosseguir' %>
+            </p>
+          EOF
+          template.result(binding)
         end
       end
     end
