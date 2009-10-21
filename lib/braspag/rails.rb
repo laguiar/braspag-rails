fa7a06b8e26691f00a22873c6cef6c309d2251ee
Diego Carrion <dc.rec1@gmail.com>
2009-10-21 20:52:36 -0200

    adicionados testes e refactorados os helpers

--- lib/braspag/rails.rb
+++ lib/braspag/rails.rb
@@ -1,29 +1,12 @@
-require File.join(File.dirname(__FILE__), 'rails_helpers')
-
 module Braspag
   module Rails
     module FormHelper
-      include Braspag::Rails::Helpers
-
-      def braspag_show_form
-        form_tag 'https://homologacao.pagador.com.br/pagador/index.asp' do
-          render :layout => "payments/show/all" do
-            block.call if block
-          end
-          html = yield
-          html.concat hidden_field_tag 'Id_Loja', '{84BE7E7F-698A-6C74-F820-AE359C2A07C2}'
-          html.concat hidden_field_tag 'crypt', '9IoLjnw4hCcHrmy6yHrKMH5B1HN9ifF72pRXZg8Iqz7wKAsCQ63IV/wsVQgVDkwOzDcGQw7sq4nin82Ci2K2y8MdfozrT4lgY8yTUmHHf/H3ChmA7g+vHkERz+/7U1DP'
-          html.concat '<p>'
-          html.concat submit_tag 'Comprar'
-          html.concat '</p>'
-        end
+      def braspag_confirmation(&block)
+        render :layout => "braspag/confirmation", &block
       end
-      def braspag_edit_form_for(route, &block)
-        form_tag route do
-          render :layout => "payments/edit/all" do
-            block.call if block
-          end
-       end
+
+      def braspag_form(&block)
+        render :layout => "braspag/form", &block
       end
     end
   end
