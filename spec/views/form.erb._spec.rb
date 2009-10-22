74be811d3b29d8b01b62294b05b8d8192389d039
Diego Carrion <dc.rec1@gmail.com>
2009-10-22 15:37:21 -0200

    especificados os campos do formulario da braspag

--- spec/views/form.erb._spec.rb
+++ spec/views/form.erb._spec.rb
@@ -14,5 +14,11 @@ describe "_form.erb" do
     it "should target braspag_path" do
       @form.should have_tag("form[@action='#{braspag_path}']")
     end
+
+    %w(VENDAID VALOR NOME CODPAGAMENTO).each do |field|
+      it "should have a #{field} field" do
+        @form.should have_tag("input[@name='#{field}']")
+      end
+    end
   end
 end
