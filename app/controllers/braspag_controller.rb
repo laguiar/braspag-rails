2a4d6722a5cbacd9101641af7dea1324c4929099
Ricardo Almeida <ricardo.almeida@gonow.com.br>
2009-10-27 21:05:04 -0200

    incluindo ajax e removendo campos que nao devem ser criptografados

--- app/controllers/braspag_controller.rb
+++ app/controllers/braspag_controller.rb
@@ -2,7 +2,11 @@ class BraspagController < ApplicationController
   def encrypt
     merchant_id = YAML.load_file("#{RAILS_ROOT}/config/braspag.yml")['merchant_id']
     @braspag = Braspag::Connection.new merchant_id, RAILS_ENV
-    puts params
-    @crypt = Braspag::Cryptography.new(@braspag).encrypt params
+
+    form_fields = {}
+    params.each do |key, value|
+      form_fields[key] = value unless ["action", "controller"].include? key
+    end
+    @crypt = Braspag::Cryptography.new(@braspag).encrypt form_fields
   end
 end
