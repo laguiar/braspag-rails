bdcf6de3e15eff43ed42c56972a167463cf854be
Ricardo Almeida <ricardo.almeida@gonow.com.br>
2009-10-27 17:48:39 -0200

    incluindo ajax para a confirmação dos dados

--- app/controllers/braspag_controller.rb
+++ app/controllers/braspag_controller.rb
@@ -2,6 +2,7 @@ class BraspagController < ApplicationController
   def encrypt
     merchant_id = YAML.load_file("#{RAILS_ROOT}/config/braspag.yml")['merchant_id']
     @braspag = Braspag::Connection.new merchant_id, RAILS_ENV
+    puts params
     @crypt = Braspag::Cryptography.new(@braspag).encrypt params
   end
 end
